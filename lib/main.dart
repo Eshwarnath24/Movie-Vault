eb46f06dd9794c7f3956ec59655fdf38fd6bb077
mahitha-kusumanchi <mahithakusumanchi0248@gmail.com>
2025-10-09 17:38:46 +0530

    integrated subscription page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ott/pages/Authorization/splashScreen.dart';
-import 'package:ott/pages/subscription/subscriptionPage.dart';
-import 'package:ott/pages/subscription/paymentPage.dart';
 
 void main() {
   runApp(const MainApp());
@@ -28,7 +26,7 @@ class MainApp extends StatelessWidget {
           border: InputBorder.none,
         ),
       ),
-      home: SubscriptionPage(),
+      home: SplashScreen(),
     );
   }
 }
